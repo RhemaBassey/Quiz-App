d48ef4c17635e9800f2accd15e791f8640893b59
unknown <bassey.rhema@yahoo.com>
2023-09-21 19:07:52 +0100

    created section for the right answer in the selectedOptions list and also enabled sumbit button functionality to display score as an alert message

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       question: "Q1",
       options: ["Q1 OP1", "Q1 OP2", "Q1 OP3", "Q1 OP4"],
+      answer: "Q1 OP1",
     },
     {
       question: "Q2",
       options: ["Q2 OP1", "Q2 OP2"],
+      answer: "Q2 OP2",
     },
     {
       question: "Q3",
       options: ["Q3 OP1", "Q3 OP2", "Q3 OP3", "Q3 OP4"],
+      answer: "Q3 OP3",
     },
     {
       question: "Q4",
       options: ["Q4 OP1", "Q4 OP2", "Q4 OP3"],
+      answer: "Q4 OP2",
     },
     {
       question: "Q5",
       options: ["Q5 OP1", "Q5 OP2", "Q5 OP3", "Q5 OP4", "Q5 OP5"],
+      answer: "Q5 OP1",
     },
   ];
+
+  var correct_answers = 0
 
   var selectedOptions = [];
   for (var i in questions) {
@@ -75,7 +82,11 @@ document.addEventListener("DOMContentLoaded", function () {
         var selectedOptionsDetails = {
           question_num: questionCount,
           option_index: this.id,
+          text: this.innerText,
+          isCorrect: this.innerText == questions[questionCount - 1].answer,
         };
+
+
 
         //removes other highlights
         for (var optionBtn of optionBtns) {
@@ -85,23 +96,21 @@ document.addEventListener("DOMContentLoaded", function () {
         //highlight selected btn
         this.classList.add("selected-btn");
 
-        // if (selectedOptions.some(option => option.question_num === selectedOptionsDetails.question_num && option.option_index === selectedOptionsDetails.option_index)) {
-        //   this.classList.add("selected-btn");
-        // }
-
+        //appends clicked option button in their appropriate slot in the selectedOptions list
         selectedOptions[selectedOptionsDetails.question_num - 1] =
           selectedOptionsDetails;
-        console.log(selectedOptions.length);
 
         // turn submit button green
-       
-        if (selectedOptions.includes('') == false && questionCount>=questionCountMax -1){
-          nextBtn.classList.add("submit-green-btn")
-          console.log("green")
-        }
 
-
+        // if (
+        //   selectedOptions.includes("") == false &&
+        //   questionCount >= questionCountMax - 1
+        // ) {
+        //   nextBtn.classList.add("submit-green-btn");
+        // }
+        submitBtn(questionCountMax -1, false)
         nextPage();
+
       });
 
       displayedOptions.appendChild(optionBtn);
@@ -146,7 +155,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (questionCount > 1) {
       questionCount--;
 
-      nextBtn.classList.remove("submit-green-btn")
+      nextBtn.classList.remove("submit-green-btn");
 
       if (questionCount == 1) {
         previousBtn.classList.add("disabled");
@@ -166,17 +175,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (questionCount < questionCountMax) {
       questionCount++;
 
-
-      if (questionCount == questionCountMax) {
-        // turn submit button green
-
-        if (selectedOptions.includes('') == false){
-          nextBtn.classList.add("submit-green-btn")
-          console.log("green")
-        }
-        nextBtn.innerHTML = "SUBMIT";
-      }
-      
       previousBtn.classList.remove("disabled");
       displayedQuestionCount.innerText = questionCount;
       num.start = questionCount;
@@ -194,6 +192,43 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   nextBtn.addEventListener("click", function () {
+
+
+    submitBtn(questionCountMax, true);
     nextPage();
+
+
   });
+
+  function submitBtn(countMax, isSubmitBtn) {
+
+    if (questionCount > countMax -1) {
+
+      //       // turn submit button green
+      if (selectedOptions.includes("") == false) {
+        nextBtn.classList.add("submit-green-btn");
+      }
+
+
+      if (questionCount == questionCountMax && isSubmitBtn == true) {
+        correct_answers = 0
+       for(var option of selectedOptions){
+
+        if(option.isCorrect){
+          correct_answers +=1 
+        }
+       }
+        alert("You got: "+correct_answers+"/"+questionCountMax)
+      }
+      // text change to submit
+
+
+    }
+    if (questionCount == questionCountMax -1) {
+      nextBtn.innerHTML = "SUBMIT";
+      if (selectedOptions.includes('') == false && isSubmitBtn){
+        nextBtn.classList.add("submit-green-btn")
+      }
+    }
+  }
 });
