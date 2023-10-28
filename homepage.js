3397417d8f3cd3a101214a9c8d49e81f436b1ce3
unknown <bassey.rhema@yahoo.com>
2023-10-28 17:01:50 +0100

    adding color and text changes

--- homepage.js
+++ homepage.js
@@ -1,26 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
   var subjectsContainer = document.getElementById("subjectsContainer");
   var subjects = [
-    "Maths",
-    "Physics",
-    "Chemistry",
-    "Biology",
-    "History",
-    "Random",
-    "extra"
+    {
+      title: "Maths",
+
+      text: " Test your mathematical skills with a range of questions covering topics like algebra, geometry, calculus, and more.",
+    },
+    {
+      title: "Physics",
+
+      text: "This subject covers areas like motion, energy, electricity, magnetism, and more. Test your understanding of the natural laws that govern our world.",
+    },
+    {
+      title: "Chemistry",
+      text: "Dive into the realm of atoms, molecules, reactions, and the periodic table to test your chemical knowledge.",
+    },
+    {
+      title: "Biology",
+      text: " Delve into the world of living organisms, their structures, functions, and ecosystems. Test your knowledge on topics ranging from cells and genetics to ecology and evolution.",
+    },
+    {
+      title: "History",
+      text: "Travel through time and test your knowledge of historical events, figures, and civilizations from various eras.",
+    },
+    {
+      title: "Random",
+      text: " Challenge yourself with a mix of questions from different subjects, providing a diverse and fun quiz experience.",
+    },
   ];
 
   for (subject of subjects) {
     var subjectCard = document.createElement("div");
-    var subjectCardImage = document.createElement("img")
-    subjectCardImage.className = 'subjectcardimage'
-    subjectCardImage.src = "https://th.bing.com/th/id/R.648daa73c4b2534b544be2d8e5eeb91a?rik=viV2AFC3%2btB60g&pid=ImgRaw&r=0"
-    subjectCardImage.alt= subject + "-poster-image"
+    var subjectCardTitle = document.createElement("h2");
+    var subjectCardImage = document.createElement("img");
+    var subjectCardText = document.createElement("p");
+    var imageName = subject.title.toLowerCase() + "-poster-image.jpg";
 
-
+    subjectCardImage.className = "subjectcardimage";
+    subjectCardImage.src = "/images/" + imageName;
+    subjectCardImage.alt = imageName;
+    subjectCardTitle.innerText = subject.title;
+    subjectCardText.innerText = subject.text;
     subjectCard.className = "card";
-    subjectCard.innerText = subject;
-    subjectCard.appendChild(subjectCardImage)
+    subjectCard.appendChild(subjectCardTitle);
+    subjectCard.appendChild(subjectCardImage);
+    subjectCard.appendChild(subjectCardText);
 
     subjectsContainer.appendChild(subjectCard);
   }
